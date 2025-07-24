ebb35ad49cfcd692f724d40af9f901920acafa6f
austinpurdie <101536676+austinpurdie@users.noreply.github.com>
2025-07-23 19:56:08 -0600

    Finishing up doubly linked list class

--- 2 Doubly Linked Lists/doubly_linked_lists.py
+++ 2 Doubly Linked Lists/doubly_linked_lists.py
@@ -11,6 +11,12 @@ class DoublyLinkedList:
         self.tail = new_node
         self.length = 1
 
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
     def append(self, value):
         new_node = Node(value)
         if self.length == 0:
@@ -72,6 +78,50 @@ class DoublyLinkedList:
                 temp = temp.next
         else:
             temp = self.tail
-            for _ in range(self.length - index):
+            for _ in range(self.length - index - 1):
                 temp = temp.prev
         return temp
+    
+    def set_value(self, index, value):
+        current_node = self.get(index)
+        if current_node is not None:
+            current_node.value = value
+            return True
+        else:
+            return False
+        
+    def insert(self, index, value):
+        # Edge case when index = 0, index = length, index out of range
+        if index < 0 or index > self.length:
+            return False
+        elif index == 0:
+            self.prepend(value)
+        elif index == self.length:
+            self.append(value)
+        else:
+            new_node = Node(value)
+            current_node = self.get(index)
+            new_node.prev = current_node.prev
+            current_node.prev.next = new_node
+            new_node.next = current_node
+            current_node.prev = new_node
+        self.length += 1
+        return True
+    
+    def remove(self, index):
+        to_remove = self.get(index)
+        if to_remove is None:
+            return None
+        elif index == 0:
+            return self.pop_first()
+        elif index == self.length - 1:
+            return self.pop()
+        else:
+            before = to_remove.prev
+            after = to_remove.next
+            before.next = after
+            after.prev = before
+            to_remove.next = None
+            to_remove.prev = None
+            self.length -= 1
+        return to_remove
